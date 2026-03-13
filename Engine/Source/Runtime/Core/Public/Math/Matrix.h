619fe5f8b672d199c5f0af8eb83c5c9d1eb52a73
wwonnn <dnjsgml0508@naver.com>
2026-03-13 16:29:39 +0900

    Chore: 폴더 정리

--- Engine/Source/Runtime/Core/Public/Math/Matrix.h
+++ Engine/Source/Runtime/Core/Public/Math/Matrix.h
@@ -1,28 +1,8 @@
 #pragma once
 
-struct FMatrix {
-  float M[4][4];
-  static const FMatrix Identity;
-};
-
-struct FVector {
-  float X;
-  float Y;
-  float Z;
-
-  float Dot(const FVector &rhs);
-  FVector Cross(const FVector &rhs);
-  float Length() const;
-  void Normalize();
-};
-
-struct FVector4 {
-  float X;
-  float Y;
-  float Z;
-  float W;
-
-  float Dot(const FVector4 &Other);
-  float Length();
-  float Length3();
+struct FMatrix
+{
+public:
+	float M[4][4];
+	static const FMatrix Identity;
 };
--- Engine/Source/Runtime/Core/Public/Matrix.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-struct FMatrix
-{
-public:
-	float M[4][4];
-};
