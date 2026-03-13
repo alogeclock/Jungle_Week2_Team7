81e3d3120e2255edc1c60f59e3d6fb4dd5577815
JunHyeop3631 <sstechangle@gmail.com>
2026-03-14 02:45:38 +0900

    feat: FGenericPlatformMath 사용가능 함수 추가

--- Engine/Source/Runtime/Core/Public/Math/GenericPlatformMath.h
+++ Engine/Source/Runtime/Core/Public/Math/GenericPlatformMath.h
@@ -4,6 +4,8 @@
 
 struct FGenericPlatformMath
 {
+	static float Sin(float Value) { return sinf(Value); }
+	static float Cos(float Value) { return cosf(Value); }
 	static float Tan(float Value) { return tanf(Value); }
 	static float Sqrt(float Value) { return sqrtf(Value); }
 	static float InvSqrt(float F) { return 1.0f / sqrtf(F); }
